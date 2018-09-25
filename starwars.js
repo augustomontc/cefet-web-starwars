34477101219c4b6b81afeeb8c7840829c3920f67
nnatsumy <natsumyiass@gmail.com>
2018-09-25 11:53:10 -0300

    Primeiro commit

--- starwars.js
+++ starwars.js
@@ -3,3 +3,12 @@
 // para carregar:
 //  - A lista de filmes
 //  - A introdução de cada filme, quando ele for clicado
+$.ajax({
+  url: 'https://swapi.co/api/films/',
+  dataType: 'json',
+  success: function(resposta) {
+    // escreve a resposta no console (para ver o que foi recebido)
+    console.log(resposta);
+    let listaFilmes = document.querySelectorAll('#movies > ul > li');
+  }
+});
